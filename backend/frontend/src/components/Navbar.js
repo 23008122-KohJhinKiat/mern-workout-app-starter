7714cbcc1954685cecfbbbed7cc3fb0f63207274
Koh Jhin Kiat <91869260+31-KohJhinKiat@users.noreply.github.com>
2025-02-01 11:47:09 +0800

    login logout

--- backend/frontend/src/components/Navbar.js
+++ backend/frontend/src/components/Navbar.js
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
+
     return (
         <header>
             <div className="container">
@@ -8,6 +16,9 @@ const Navbar = () => {
                     <h1>Workout Buddy</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
                     <div>
                         <Link to="/login">Log in</Link>
                         <Link to="/signup">Sign up</Link>
